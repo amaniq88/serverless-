727f28bd1caf8acb4f77d2bf7710c862492ef972
amaniq88 <amani.musallamq88@gmail.com>
2022-04-11 11:04:54 +0300

    weather

--- api/wethear.py
+++ api/wethear.py
@@ -1,7 +1,5 @@
 from http.server import BaseHTTPRequestHandler
-from datetime import datetime
-from hijri_converter import Hijri, Gregorian
-
+import requests, json
 
 
 class handler(BaseHTTPRequestHandler):
@@ -14,15 +12,37 @@ class handler(BaseHTTPRequestHandler):
 
 
 # Convert a Hijri date to Gregorian
-    g = Hijri(1403, 2, 17).to_gregorian()
-
-# Convert a Gregorian date to Hijri
-    h = Gregorian(1982, 12, 2).to_hijri()
-    self.wfile.write(g.encode())
-    self.wfile.write(h.encode())
-
-
-
+  # importing requests and json
+# base URL
+    BASE_URL = "https://api.openweathermap.org/data/2.5/weather?"
+    CITY = "Amman"
+    API_KEY = "d4d48bc4478ea6f2ed41d2d7135001cd"
+    # upadting the URL
+    URL = BASE_URL + "q=" + CITY + "&appid=" + API_KEY
+    # HTTP request
+    response = requests.get(URL)
+    # checking the status code of the request
+    if response.status_code == 200:
+      # getting data in the json format
+      data = response.json()
+      # getting the main dict block
+      main = data['main']
+      # getting temperature
+      temperature = main['temp']
+      # getting the humidity
+      humidity = main['humidity']
+      # getting the pressure
+      pressure = main['pressure']
+      # weather report
+      report = data['weather']
+      self.wfile.write(f"{CITY:-^30}".encode())
+      # print(f"Temperature: {temperature}")
+      # print(f"Humidity: {humidity}")
+      # print(f"Pressure: {pressure}")
+      # print(f"Weather Report: {report[0]['description']}")
+    else:
+      # showing the error message
+      print("Error in the HTTP request")
 
     return
 
